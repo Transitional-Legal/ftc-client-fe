db6dff9369756bdbda83e9fe116b5e269715cfe2
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-06-13 12:06:04 +1000

    closes #5

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Nav from "components/layout/Nav";
-import Footer from "components/layout/Footer";
 import "./Layout.scss";
 
 const Layout = ({
   header,
-  footer,
   children,
   noFooter,
   noHeader,
@@ -18,7 +16,6 @@ const Layout = ({
       {noHeader || header || <Nav activeTab={activeTab} links={navLinks} />}
     </header>
     <main {...props}>{children}</main>
-    <footer>{noFooter || footer || <Footer />}</footer>
   </>
 );
 
