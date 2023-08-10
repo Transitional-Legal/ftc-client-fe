6c038370ae231eedb5914086e1e95fc52b1504f2
tommycp3 <thomasmillerprivate@gmail.com>
2023-08-10 13:18:59 +1000

    added status

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -27,6 +27,7 @@ export default function Summary({ summary, setShow }) {
 	return (
 		<Container>
 			<h3>Where things are at ...</h3>
+			<p>Pending Case Formalisation</p>
 			<p>{summary?.summary}</p>
 			<h3>Next steps ...</h3>
 			<Form.Group>
