8d179e1707d11d9aa6f250d0985a72b381866103
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2023-12-18 09:56:31 +1000

    feat: fix react dom 6 issues

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "pages/LoginPage";
 import RegisterPage from "pages/RegisterPage";
-import AuthRoute from "components/auth/AuthRoute";
-import Dashboard from "pages/Dashboard";
+// import AuthRoute from "components/auth/AuthRoute";
+// import Dashboard from "pages/Dashboard";
 import ResetPasswordPage from "pages/ResetPasswordPage";
 import VerifyEmailPage from "pages/VerifyEmailPage";
 // import ProfilePage from "pages/ProfilePage";
@@ -12,16 +12,16 @@ import DocumentRequest from "pages/DocumentRequest";
 
 const Router = () => (
 	<BrowserRouter>
-		<RefreshLoginModal />
+		{/* <RefreshLoginModal /> */}
 		<Routes>
-			<Route path="/auth/resetpassword/:token" component={ResetPasswordPage} />
-			<Route path="/auth/resetpassword" component={ResetPasswordPage} />
-			<Route path="/verify/email/:token" component={VerifyEmailPage} />
-			<Route path="/login" component={LoginPage} />
-			<Route path="*/register" component={RegisterPage} />
-			{/* <AuthRoute path="/profile" component={ProfilePage} /> */}
-			<Route path="/documentrequest" component={DocumentRequest} />
-			<AuthRoute path="/" component={Dashboard} allowUnverified />
+			{/* <Route path="/auth/resetpassword/:token" element={ResetPasswordPage} />
+			<Route path="/auth/resetpassword" element={ResetPasswordPage} />
+			<Route path="/verify/email/:token" element={VerifyEmailPage} />
+			<Route path="/login" element={LoginPage} /> */}
+			
+			<Route path="*/register" element={ <RegisterPage/> } />
+			<Route path="/documentrequest" element={ <DocumentRequest/> } />
+			{/* <AuthRoute path="/" component={Dashboard} allowUnverified /> */}
 		</Routes>
 	</BrowserRouter>
 );
