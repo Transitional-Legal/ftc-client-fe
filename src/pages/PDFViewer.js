c304bb56fea9e415fea7d9965a3e5257bcd7db7d
Alex Miller <alexmiller@Alexs-MacBook-Pro.local>
2024-01-13 17:24:51 +1000

    Added script to copy webviewer to public directory and updated PDFViewer component path and style

--- src/pages/PDFViewer.js
+++ src/pages/PDFViewer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import Layout from "components/layout/Layout";
 import WebViewer from "@pdftron/webviewer";
+import { makeHeightFlexible } from "react-vis";
 
 const PDFViewer = () => {
 	const viewerDiv = useRef(null);
@@ -8,7 +9,7 @@ const PDFViewer = () => {
 
 	useEffect(() => {
 		WebViewer({
-			path: '/public',
+			path: '/webviewer',
 			initialDoc: "/statement.pdf",
 			licenseKey: "demo:1705106612291:7f6eb60903000000009980cbbe3fa4805a79f378f3a90724238516e2da"
 		}, viewerDiv.current).then(instance => { });
@@ -18,7 +19,7 @@ const PDFViewer = () => {
 		<Layout navLinks={[]}>
 			<div className="container py-5">
 				<h1>Create a new Financial Statement</h1>
-				<div className="webviewer" ref={viewerDiv}></div>
+				<div className="webviewer" ref={viewerDiv} style={{height: "100vh"}}></div>
 			</div>
 		</Layout>
 	);
